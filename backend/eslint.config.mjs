b332b1df5673fd942409be87f479ea2ae1f3bf8c
Wan <158989003+ridhwanmnr@users.noreply.github.com>
2024-09-08 07:26:02 +0800

    minor changes

--- backend/eslint.config.mjs
+++ backend/eslint.config.mjs
@@ -7,23 +7,39 @@ export default [
   {
     files: ['**/*.mjs', '**/*.js'],
     languageOptions: {
-      sourceType: 'module'
+      sourceType: 'module',
     },
     plugins: {
-      prettier
+      prettier,
     },
-    extends: [
-      pluginJs.configs.recommended,
-      configPrettier
-    ],
+    extends: [pluginJs.configs.recommended, configPrettier],
     rules: {
+      // Separate import groups with newline by section
+      'import/order': [
+        'error',
+        {
+          groups: [
+            'builtin',
+            'external',
+            'internal',
+            'parent',
+            'sibling',
+            'index',
+            'unknown',
+          ],
+          'newlines-between': 'always',
+        },
+      ],
+      'no-useless-return': 1,
+      'newline-before-return': 1,
       'prettier/prettier': ['error'],
-      'quotes': ['error', 'single']
-    }
+      'quotes': ['error', 'single'],
+      'react/prop-types': 1,
+    },
   },
   {
     languageOptions: {
-      globals: { ...globals.browser, ...globals.node }
-    }
-  }
+      globals: { ...globals.browser, ...globals.node },
+    },
+  },
 ];
