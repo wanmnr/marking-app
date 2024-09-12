b76804f068092d7dbee72f904bfe9353263884ed
Wan <158989003+ridhwanmnr@users.noreply.github.com>
2024-09-12 15:45:03 +0800

    feat(sheet): Add new 'sheetTitle', 'subjectId', 'studentName', 'studentMatrixNumber', 'classId', 'sections'  fields to sheet model.

--- backend/models/Sheet.js
+++ backend/models/Sheet.js
@@ -1,25 +1,37 @@
-// Backend - /backend/models/Sheet.js
-
 const mongoose = require('mongoose'); // Import Mongoose
 
 // Define the schema for a Marking Sheet
 const SheetSchema = new mongoose.Schema({
-  title: { type: String, required: true }, // Title of the marking sheet
-  classGroup: { type: String, required: true }, // Class group associated with the sheet
-  subject: {
+  sheetTitle: { type: String, required: true }, // Title of the marking sheet
+  subjectId: {
     type: mongoose.Schema.Types.ObjectId,
     ref: 'Subject',
     required: true,
   }, // Reference to the Subject model
+  studentName: { type: String, required: true }, // Student name
+  studentMatrixNumber: { type: String, required: true }, // Student Matrix Number
+  classId: { type: Number, required: true }, // Class ID associated with the sheet
+  sections: [
+    {
+      sectionId: { type: Number, required: true },
+      sectionName: { type: String, required: true },
+      questions: [
+        {
+          questionId: { type: Number, required: true },
+          questionText: { type: String, required: true },
+          maxScore: { type: Number, required: true },
+          score: { type: Number, default: null }, // Initially null until marked
+        },
+      ],
+    },
+  ],
+  fileUrl: { type: String, required: true }, // URL of the uploaded file (PDF)
+  uploadedAt: { type: Date, default: Date.now }, // Timestamp when the sheet was uploaded
   status: {
     type: String,
     enum: ['New', 'In Progress', 'Completed'],
     default: 'New',
   }, // Status of the marking process
-  score: { type: Number, default: 0 }, // Score obtained
-  dueDate: { type: Date, required: true }, // Due date for completing the marking
-  fileUrl: { type: String, required: true }, // URL of the uploaded file (PDF)
-  uploadedAt: { type: Date, default: Date.now }, // Timestamp when the sheet was uploaded
 });
 
 // Export the Sheet model based on the SheetSchema
