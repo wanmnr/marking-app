40cd74a964d7518f50c92aba7ab9719baac907d6
Wan <158989003+ridhwanmnr@users.noreply.github.com>
2024-09-14 23:03:42 +0800

    feat(routes): Introduce AppRoutes component to manage application routes - Created a new `AppRoutes` component in the `src/routes` folder to handle the application's routes - Defined routes for public pages (Login, SignUp) and private pages (Dashboard, UploadSheet) - Implemented `PrivateRoute` and `PublicRoute` components to handle route authorization - Included placeholders for future routes (SubjectManagement, ExportResults, Settings) - Improved the organization and maintainability of the application's routing logic

---
+++ frontend/src/routes/AppRoutes.jsx
@@ -0,0 +1,59 @@
+// src/routes/AppRoutes.jsx
+import React from 'react';
+import { Routes, Route } from 'react-router-dom';
+import Layout from '../components/Layout';
+import Login from '../pages/Login';
+import SignUp from '../pages/SignUp';
+import Dashboard from '../pages/Welcome';
+import UploadSheet from './pages/UploadSheet';
+// import SubjectManagement from './pages/SubjectManagement';
+// import ExportResults from './pages/ExportResults';
+// import Settings from './pages/Settings';
+import NotFound from '../pages/NotFound';
+import PrivateRoute from '../routes/PrivateRoute';
+import PublicRoute from '../routes/PublicRoute';
+
+const AppRoutes = () => (
+  <Routes>
+    <Route path="/login" element={
+        <PublicRoute>
+            <Login />
+        </PublicRoute>
+    } />
+    <Route path="/signup" element={
+        <PublicRoute>
+            <SignUp />
+        </PublicRoute>
+    } />
+    <Route path="/" element={<Layout />}>
+      <Route index element={
+        <PrivateRoute>
+            <Dashboard />
+        </PrivateRoute>
+      } />
+      <Route path="upload" element={
+        <PrivateRoute>
+            <UploadSheet />
+        </PrivateRoute>
+      } />
+      {/* <Route path="subjects" element={
+          <PrivateRoute>
+            <SubjectManagement />
+            </PrivateRoute>
+          } />
+        <Route path="export" element={
+          <PrivateRoute>
+            <ExportResults />
+          </PrivateRoute>
+        } />
+        <Route path="settings" element={
+          <PrivateRoute>
+            <Settings />
+          </PrivateRoute>
+        } /> */}
+      <Route path="*" element={<NotFound />} />
+    </Route>
+  </Routes>
+);
+
+export default AppRoutes;
